e2e41b309058d306522c89df25dad524502d5ed1
hoxey2 <hoxey2@gmail.com>
2023-05-08 00:29:18 +0900

    remove: 주석제거

--- travel_subscribe/function.js
+++ travel_subscribe/function.js
@@ -1,11 +1,5 @@
+// 스크롤 시 floating button 
 $(document).on("scroll", function () {
-    // let pos = $(window).scrollTop();
-    // if(pos == 0){
-    //     $(".travelSubscribe_floatBtn_wrap").stop().animate({right:"-138px"},"fast");
-    // }else {
-    //     $(".travelSubscribe_floatBtn_wrap").stop().animate({right:"0"},"fast");
-    // }
-
     let floatBtnPos = ($(window).scrollTop() === 0)? "-138px": "0";
     $(".travelSubscribe_floatBtn_wrap").stop().animate({right: floatBtnPos},"fast");
 });
