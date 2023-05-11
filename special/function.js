6023d38be22512bde829758bfdeb88b06c634879
hoxey2 <hoxey2@gmail.com>
2023-05-11 23:23:36 +0900

    feat: 테마 기능 구현 완

--- special/function.js
+++ special/function.js
@@ -1,5 +1,11 @@
+// 경로 선택
+$(".main_specialRoute_wrap h5 a").on("click", ()=>{
+    $(".main_specialRoute_wrap ul").stop().slideToggle();
+})
+
 // go to top 기능
 var topBtn = document.querySelector(".special_float_btn")
 topBtn.onclick = () => {
     window.scrollTo({top:0, behavior:"smooth"});
 }
+
