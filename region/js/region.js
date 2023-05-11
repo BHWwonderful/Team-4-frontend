4cbddee73e6d0030a2b1a8c34a8ac058e969b005
JaeHoon <ljhoon0925@naver.com>
2023-05-11 21:51:31 +0900

    지역90퍼완료

--- region/js/region.js
+++ region/js/region.js
@@ -1,3 +1,8 @@
 $(".region_guideList li:eq(2)").click(function() {
     $(".region_3depthList").slideToggle("fast");
 })
+
+$(".region_city_box a").click(function() {
+    $(".region_city_box a").removeClass("region_city_box_active");
+    $(this).addClass("region_city_box_active");
+})
