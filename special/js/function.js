0a92589dadb53fcc3aace3754f02daf573236255
hoxey2 <hoxey2@gmail.com>
2023-05-12 18:20:30 +0900

    feat: special 4depth 진행중

--- special/js/function.js
+++ special/js/function.js
@@ -1,6 +1,6 @@
 // 경로 선택
 $(".main_specialRoute_wrap h5 a").on("click", ()=>{
-    $(".main_specialRoute_wrap ul").stop().slideToggle();
+    $(".main_specialRoute_wrap ul").slideToggle();
 })
 
 // go to top 기능
