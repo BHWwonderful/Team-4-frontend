fa92bfc58a4bb2d83eef667b3ad5d3def4adc1c9
hoxey2 <hoxey2@gmail.com>
2023-05-15 18:16:34 +0900

    패키지 마크업 완료

--- package&theme/js/function.js
+++ package&theme/js/function.js
@@ -1,10 +1,10 @@
 // 경로 선택
-$(".main_specialRoute_wrap h5 a").on("click", ()=>{
-    $(".main_specialRoute_wrap ul").slideToggle();
+$(".main_themeRoute_wrap h5 a").on("click", ()=>{
+    $(".main_themeRoute_wrap ul").slideToggle();
 })
 
 // go to top 기능
-var topBtn = document.querySelector(".special_float_btn")
+var topBtn = document.querySelector(".theme_float_btn")
 topBtn.onclick = () => {
     window.scrollTo({top:0, behavior:"smooth"});
 }
