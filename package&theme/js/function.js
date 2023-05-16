6667eb96203afe0f6aa342df811222ac23612aa2
hoxey2 <hoxey2@gmail.com>
2023-05-17 00:40:29 +0900

    jq slider 진행

--- package&theme/js/function.js
+++ package&theme/js/function.js
@@ -10,26 +10,30 @@ topBtn.onclick = () => {
 }
 
 // sample 슬라이드
-left = $(".sample_slideImg_box").css("left");
-currentSlide = 1;
-sliderWidth = 1180
-$(".sample_slideBtn_next").on("click", function(){
-    currentSlide++;
-    $(".sample_slideImg_box").stop().animate({left: -currentSlide * sliderWidth+"px"});
-    left = $(".sample_slideImg_box").css("left");
-    console.log(left)
-    if(left == -4720){
-        $(".sample_slideImg_box").css("left", "-1180px");
-    }
-})
-$(".sample_slideBtn_prev").on("click", function(){
-    currentSlide--;
-    $(".sample_slideImg_box").stop().animate({left: -currentSlide * sliderWidth+"px"})
-    left = $(".sample_slideImg_box").css("left");
-    console.log(left)
-    if(left == 0){
-        $(".sample_slideImg_box").css("left", "-3540px");
-    }
-})
+$(document).ready(()=>{
+    currentSlide = 1;
+    sliderWidth = 1180
+    $(".sample_slideBtn_next").on("click", function(){
+        if( currentSlide <= 3){
+            currentSlide++;
+            $(".sample_slideImg_box").stop().animate({left: -currentSlide * sliderWidth+"px"});
+        }else if(currentSlide >= 4){
+            // $(".sample_slideImg_box").stop().animate({left: -currentSlide * sliderWidth+"px"});
+            currentSlide = 1; 
+            $(".sample_slideImg_box").css("left", "-1180px");
+        }
+    })
+    $(".sample_slideBtn_prev").on("click", function(){
+        if(currentSlide >= 1){
+            currentSlide--;
+            $(".sample_slideImg_box").stop().animate({left: -currentSlide * sliderWidth+"px"});
+        }else if(currentSlide <= 0){
+            // $(".sample_slideImg_box").stop().animate({left: -currentSlide * sliderWidth+"px"});
+            currentSlide = 3;
+            $(".sample_slideImg_box").css("left", "-3540px");
+        }
+    })
+});
+
 
 
