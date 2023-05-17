9f268a3fd51c92f312339ae4d2b7aade757cbdca
hoxey2 <hoxey2@gmail.com>
2023-05-17 15:18:48 +0900

    jq slide 수정

--- package&theme/js/function.js
+++ package&theme/js/function.js
@@ -14,7 +14,6 @@ $(document).ready(() => {
     currentSlide = 1;
     sliderWidth = 1180;
     slideCount = 3;
-  
     $(".sample_slideBtn_next").on("click", function() {
       currentSlide++;
       if (currentSlide > slideCount) {
@@ -34,36 +33,52 @@ $(document).ready(() => {
     });
   });
 
-// package 슬라이드
+// package_travel_now 슬라이드
 $(document).ready(() => {
+    origin = 1770;
     currentSlide = 0;
     sliderWidth = 295;
     slideCount = 6;
-    $(".package_sliderBtn_next").on("click", function() {
-        right = $(".package_slider").css("right");
+    $(".package_travel_now .package_sliderBtn_next").on("click", function() {
       currentSlide++;
       if (currentSlide > slideCount) {
         currentSlide = 1;
-        $(".package_slider").css("right", "1770px");
-        console.log(right)
-        // alert("next: "+currentSlide);
+        $(".package_travel_now .package_slider").css("right", "1770px");
     }
-    $(".package_slider").stop().animate({ right: 1770 + currentSlide * sliderWidth + "px" }, "slow");
-    console.log(right);
-    console.log("next: "+currentSlide);
+    $(".package_travel_now .package_slider").stop().animate({ right: origin + currentSlide * sliderWidth + "px" }, "slow");
     });
-    $(".package_sliderBtn_prev").on("click", function() {
-    right = $(".package_slider").css("right");
+    $(".package_travel_now .package_sliderBtn_prev").on("click", function() {
       currentSlide--;
       if (currentSlide < -slideCount) {
         currentSlide = -1;
-        $(".package_slider").css("right", "1770px");
-        console.log(right)
-        // alert("prev :"+currentSlide);
+        $(".package_travel_now .package_slider").css("right", "1770px");
     }
-    $(".package_slider").stop().animate({ right: 1770 + currentSlide * sliderWidth + "px" }, "slow");
-    console.log(right)
-    console.log("prev :"+currentSlide);
+    $(".package_travel_now .package_slider").stop().animate({ right: origin + currentSlide * sliderWidth + "px" }, "slow");
+   
+    });
+  });
+
+// package_wellness_travel슬라이드
+$(document).ready(() => {
+    origin = 1770;
+    w_currentSlide = 0;
+    sliderWidth = 295;
+    slideCount = 6;
+    $(".package_wellness_travel .package_sliderBtn_next").on("click", function() {
+      w_currentSlide++;
+      if (w_currentSlide > slideCount) {
+        w_currentSlide = 1;
+        $(".package_wellness_travel .package_slider").css("right", "1770px");
+    }
+    $(".package_wellness_travel .package_slider").stop().animate({ right: origin + w_currentSlide * sliderWidth + "px" }, "slow");
+    });
+    $(".package_wellness_travel .package_sliderBtn_prev").on("click", function() {
+        w_currentSlide--;
+      if (w_currentSlide < -slideCount) {
+        w_currentSlide = -1;
+        $(".package_wellness_travel .package_slider").css("right", "1770px");
+    }
+    $(".package_wellness_travel .package_slider").stop().animate({ right: origin + w_currentSlide * sliderWidth + "px" }, "slow");
     });
   });
   
