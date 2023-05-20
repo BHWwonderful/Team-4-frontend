8416aed0fa1c5a553ba3c0a81e42d1e9f6418c01
inha <inhaii@naver.com>
2023-05-20 19:59:24 +0900

    Add: 옵션박스 css 변경 , 토탈 카운트 랜더링

--- event/js/event.js
+++ event/js/event.js
@@ -1,42 +1,36 @@
-$(document).ready(function(){
+$(document).ready( function(){
     // params feature
     $(".header").load("../header/header.html");
-
-
-    // const resultData = $.getJSON( "./data/event.json", function(data) {
-    //     console.log(data.data)
-    //     console.log("json 데이터 길어 " , data.data.length);
-    //     for(let i =0; i<data.data.length; i++){
-    //         $('.event_main_board_content_wrap')
-    //         .append(
-    //             `<li>
-    //                 <div class="event_content_img">
-    //                     <img src="images/78d1da9b-f128-4aa1-a293-e95f5b12b394.png" alt="">
-    //                 </div>
-    //                 <div class="event_content">
-    //                     <p class="title"></p>
-    //                     <p class="content"></p>
-    //                     <div class="event_main_bottom">
-    //                         <span class="writer">작성자</span>
-    //                         <b class="createdBy"></b>
-    //                         <span class="writerDate">작성일</span>
-    //                         <span></span>
-    //                     </div>
-    //                 </div>
-    //             </li>`
-    //         )
-    //     }
-    //     for(let i = 0; i<data.data.length; i++){
-    //        let test =  $('.event_main_board_content_wrap li').eq(i)
-    //        test[0].childNodes[3].childNodes[1].innerText = data.data[i].title
-    //        test[0].childNodes[3].childNodes[3].innerText = data.data[i].content
-    //        test[0].childNodes[3].childNodes[5].childNodes[3].innerText = data.data[i].createdBy           
-    //        test[0].childNodes[3].childNodes[5].childNodes[7].innerText = data.data[i].createdAt           
-    //         console.log(test[0].childNodes[3].childNodes[5].childNodes);
-    //     }
-    //     // $(".title").text(data.data[0].title)
-    // })
-
-
-    // console.log(resultData)
+    var totalCount = 0;
+    const resultData =  $.getJSON( "./data/event.json", function(data) {
+        console.log(data.data)
+        console.log("json 데이터 길이 " , data.data.length);
+        totalCount = data.data.length
+        $(".event_main_total_content").children('span').append(1+"/21 "+"검색결과 총 <font color='red'>"+totalCount+"</font>건이 검색 되었습니다.</span>")
+         for(let i =0; i<data.data.length; i++){
+            $('.event_main_board_content_wrap')
+            .append(
+                `<li>
+                    <div class="event_content_img">
+                        <img src="images/${data.data[i].img}" alt="">
+                    </div>
+                    <div class="event_content">
+                        <p class="title">${data.data[i].title}</p>
+                        <p class="content">${data.data[i].content}</p>
+                        <div class="event_main_bottom">
+                            <span class="writer">작성자</span>
+                            <b class="createdBy">${data.data[i].createdBy}</b>
+                            <span class="writerDate">작성일</span>
+                            <span>${data.data[i].createdAt}</span>
+                        </div>
+                    </div>
+                </li>
+                <hr>`
+            )
+        }
+    }) 
+    $("#event_gnb_li").children('ul:eq(0)').css("display","none")
+    $("#event_gnb_li").click(function () {
+        $("#event_gnb_li").children('ul:eq(0)').slideToggle(500)
+    })
 })
