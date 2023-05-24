cde877e0365ff5db2c357bdb8248b3503c173311
GyeungHoon <ghp0415@naver.com>
2023-05-24 13:21:57 +0900

    지역필터 페이징연동 완성

--- festival/js/festival.js
+++ festival/js/festival.js
@@ -1,25 +1,171 @@
 var region = document.querySelectorAll('.festival_region_fillter_box button');
-for (var i = 0; i < region.length; i++) {
-    region[i].addEventListener('click', function (e) {
-        setEventList(e.target.value);
-        console.log(e.target.value);
 
-    })
-}
+$(document).ready(function () {
+    for (var i = 0; i < region.length; i++) {
+        region[i].addEventListener('click', function (e) {
+            setEventList(e.target.value);
+            function setEventList(region) {
+                $('.festival_list_content_box').children().remove()
+                $.get('https://gist.githubusercontent.com/GyeungHoon/483112e427915938240c7d2ab9ed59b2/raw/579100bba55992cad40e360704bffa16bcc07c71/festival.json').done(function (data) {
 
-function setEventList(eventStatus) {
+                    for (let i = 0; i < 100; i++) {
+                        if (region == "전체") {
+                            document.getElementById("festival_list_content_box").innerHTML +=
+                                `
+                        <ul>
+                        <li>
+                            <div class="festival_list_img_box">
+                                <a href="/festival/festivalDetail.html?${i}" >
+                                    <img src="../festival/images/festival_img100/${JSON.parse(data)[i].명칭}_1_공공3유형.png" alt="">
+                                </a>
+                            </div>
+                            <div class="festival_list_text_box">
+                                <p><span>행사기간</span>${JSON.parse(data)[i].행사시작일}~${JSON.parse(data)[i].행사종료일}</p>
+                                <a href="/festival/festivalDetail.html?${i}">    
+                                    <h4>${JSON.parse(data)[i].명칭}</h4>
+                                </a>
+                                <div>
+                                    <p>지역 : ${JSON.parse(data)[i].관리자}</p>
+                                    <p>장소 : ${JSON.parse(data)[i].행사장소}</p>
+                                    <p>연락처 : ${JSON.parse(data)[i].주최자연락처}</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    `
+                        } else if (JSON.parse(data)[i].주소.slice(0, 2) == region || JSON.parse(data)[i].주소.slice(0, 4) == region) {
+                            document.getElementById("festival_list_content_box").innerHTML +=
+                                `
+                            <ul>
+                            <li>
+                                <div class="festival_list_img_box">
+                                    <a href="/festival/festivalDetail.html?${i}" >
+                                        <img src="../festival/images/festival_img100/${JSON.parse(data)[i].명칭}_1_공공3유형.png" alt="">
+                                    </a>
+                                </div>
+                                <div class="festival_list_text_box">
+                                    <p><span>행사기간</span>${JSON.parse(data)[i].행사시작일}~${JSON.parse(data)[i].행사종료일}</p>
+                                    <a href="/festival/festivalDetail.html?${i}">    
+                                        <h4>${JSON.parse(data)[i].명칭}</h4>
+                                    </a>
+                                    <div>
+                                        <p>지역 : ${JSON.parse(data)[i].관리자}</p>
+                                        <p>장소 : ${JSON.parse(data)[i].행사장소}</p>
+                                        <p>연락처 : ${JSON.parse(data)[i].주최자연락처}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        `
 
-    $('.festival_list_content_box ').children().remove()
-    $.get('https://gist.githubusercontent.com/GyeungHoon/483112e427915938240c7d2ab9ed59b2/raw/579100bba55992cad40e360704bffa16bcc07c71/festival.json').done(function (data) {
+                            localStorage.setItem('data', JSON.stringify(data));
+                        }
 
-        for (let i = 0; i < 100; i++) {
-            if (JSON.parse(data)[i].주소 == eventStatus) {
-                listRender(data.data[i])
+                    }
+
+
+
+                    const rowsPerPage = 4;
+                    const rows = document.querySelectorAll('#festival_list_content_box ul li');
+                    const rowsCount = rows.length;
+                    const pageCount = Math.ceil(rowsCount / rowsPerPage);
+                    const numbers = document.querySelector('#numbers');
+                    const prevPageBtn = document.querySelector('.pagination .fa-arrow-left');
+                    const nextPageBtn = document.querySelector('.pagination .fa-arrow-right');
+
+                    let pageActiveIdx = 0; //현재 보고 있는 페이지그룹 번호
+                    let currentPageNum = 0;// 현재 보고 있는 페이지네이션 번호
+                    let maxPageNum = 5; // 페이지그룹 최대 개수
+                    $('#numbers').children().remove();
+                    for (let i = 1; i <= pageCount; i++) {
+                        numbers.innerHTML += `<li><a href="#none" onclick="window.scrollTo(0,0);">${i}</a></li>`
+                    }
+
+                    const numberBtn = numbers.querySelectorAll('a');
+
+                    // 페이지네이션 번호 감추기
+                    for (nb of numberBtn) {
+                        nb.style.display = 'none';
+                    }
+
+
+                    numberBtn.forEach((item, idx) => {
+                        item.addEventListener('click', (e) => {
+                            e.preventDefault();
+
+                            //테이블 출력 함수
+                            displayRow(idx);
+                        });
+                    });
+                    function displayRow(idx) {
+
+                        let start = idx * rowsPerPage;
+                        let end = start + rowsPerPage;
+                        let rowsArray = [...rows];
+
+                        for (ra of rowsArray) {
+                            ra.style.display = 'none';
+                        }
+                        let newRows = rowsArray.slice(start, end);
+                        for (nr of newRows) {
+                            nr.style.display = '';
+                        }
+                        for (nb of numberBtn) {
+                            nb.classList.remove('active');
+                        }
+                        numberBtn[idx].classList.add("active");
+                    }//displayRow
+                    displayRow(0);
+
+                    //페이지네이션 그룹 표시 함수
+                    function displayPage(num) {
+                        // 페이지네이션 번호 감추기
+                        for (nb of numberBtn) {
+                            nb.style.display = 'none';
+                        }
+                        let totalPageCount = Math.ceil(pageCount / maxPageNum);
+                        let pageArr = [...numberBtn];
+                        let start = num * maxPageNum;
+                        let end = start + maxPageNum;
+                        let pageListArr = pageArr.slice(start, end);
+
+                        for (let item of pageListArr) {
+                            item.style.display = 'block';
+                        }
+                        if (pageActiveIdx == 0) {
+                            prevPageBtn.style.display = 'none';
+                        } else {
+                            prevPageBtn.style.display = 'block';
+                        }
+                        if (pageActiveIdx == totalPageCount - 1) {
+                            nextPageBtn.style.display = 'none';
+                        } else {
+                            nextPageBtn.style.display = 'block';
+                        }
+
+                    }
+                    displayPage(0);
+
+                    nextPageBtn.addEventListener('click', () => {
+                        let nextPageNum = pageActiveIdx * maxPageNum + maxPageNum;
+                        displayRow(nextPageNum);
+                        ++pageActiveIdx;
+                        displayPage(pageActiveIdx);
+                    });
+
+                    prevPageBtn.addEventListener('click', () => {
+                        let nextPageNum = pageActiveIdx * maxPageNum - 1;
+                        displayRow(nextPageNum);
+                        --pageActiveIdx;
+                        displayPage(pageActiveIdx);
+                    });
+
+                })
             }
-        }
 
-    })
-}
+        })
+    }
+});
 $(document).ready(function () {
     $.get('https://gist.githubusercontent.com/GyeungHoon/483112e427915938240c7d2ab9ed59b2/raw/579100bba55992cad40e360704bffa16bcc07c71/festival.json').done(function (data) {
 
@@ -51,7 +197,7 @@ $(document).ready(function () {
             localStorage.setItem('data', JSON.stringify(data));
         }
 
-        const rowsPerPage = 10;
+        const rowsPerPage = 4;
         const rows = document.querySelectorAll('#festival_list_content_box ul li');
         const rowsCount = rows.length;
         const pageCount = Math.ceil(rowsCount / rowsPerPage);
@@ -61,7 +207,7 @@ $(document).ready(function () {
 
         let pageActiveIdx = 0; //현재 보고 있는 페이지그룹 번호
         let currentPageNum = 0;// 현재 보고 있는 페이지네이션 번호
-        let maxPageNum = 4; // 페이지그룹 최대 개수
+        let maxPageNum = 5; // 페이지그룹 최대 개수
 
         for (let i = 1; i <= pageCount; i++) {
             numbers.innerHTML += `<li><a href="#none" onclick="window.scrollTo(0,0);">${i}</a></li>`
@@ -148,4 +294,3 @@ $(document).ready(function () {
 
     });
 });
-
