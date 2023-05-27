da637aae54161f52ae798127d199c4032f923070
디히 <deardich@gmail.com>
2023-05-28 02:49:34 +0900

    css 수정

--- travelMap/function.js
+++ travelMap/function.js
@@ -1,7 +1,24 @@
-// cont_wrap 숨기기
-$(".travelMap_btn_wrap img").on("click", function(){
-    // $(".travelMap_category_box").toggleClass("animated")
-    $(".travelMap_cont_wrap").toggleClass("animated")
-    $(this).toggleClass("btn_change")
-    $(this).parent().toggleClass("btn_move")
-})
+$(document).ready(function () {
+	// cont_wrap 숨기기
+	$(".travelMap_btn_wrap img").on("click", function () {
+		$(".travelMap_cont_wrap").toggleClass("animated");
+		$(this).toggleClass("btn_change");
+		$(this).parent().toggleClass("btn_move");
+	});
+	control_mouse();
+
+	$(".travelMap_cont_box").on("scroll", function () {});
+});
+
+// 마우스 제어 (우클릭/ 드래그 차단)
+function control_mouse() {
+	$(document).bind("contextmenu", function (e) {
+		return false;
+	});
+	$(document).bind("selectstart", function () {
+		return false;
+	});
+	$(document).bind("dragstart", function () {
+		return false;
+	});
+}
